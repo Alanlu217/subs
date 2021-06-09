9191fe05318d75ae595595e75f5ebfdc7ef783ae
Alan Lu <alanlu2468@gmail.com>
2021-06-09 18:55:07 +0930

    Updated cameraTest to easily merge into controller.py

--- cameraTest.py
+++ cameraTest.py
@@ -1,40 +1,40 @@
 import pygame
-from pygame.locals import *
 import cv2
 import numpy
 
-color=True#True#False
-camera_index = 0
-camera=cv2.VideoCapture(camera_index)
-camera.set(3,640)
-camera.set(4,480)
-
 #This shows an image weirdly...
 screen_width, screen_height = 640, 480
-screen=pygame.display.set_mode((screen_width,screen_height))
 
-def getCamFrame(color,camera):
-  retval,frame=camera.read()
-  frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-  if not color:
-    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    frame=cv2.cvtColor(frame,cv2.COLOR_GRAY2RGB)
-  frame=numpy.rot90(frame)
-  frame=pygame.surfarray.make_surface(frame)
-  return frame
+class camera():
+  def __init__(self, camera_index, color, size):
+    self.camera = cv2.VideoCapture(camera_index)
+    self.camera.set(3, size[0])
+    self.camera.set(4, size[1])
+    self.color = color
 
-def blitCamFrame(frame,screen):
-  screen.blit(frame,(0,0))
-  return screen
+  def getCamFrame(self):
+    retval,frame = self.camera.read()
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    if not self.color:
+      frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+      frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2RGB)
+    frame = numpy.rot90(frame)
+    frame = pygame.surfarray.make_surface(frame)
+    return frame
 
+  def blitCamFrame(self, screen, xy):
+    screen.blit(self.getCamFrame(), xy)
 
-running=True
+cam = camera(0, True, (480, 360))
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+running = True
 while running:
   for event in pygame.event.get(): #process events since last loop cycle
-    if event.type == KEYDOWN:
-      running=False
+    if event.type == pygame.KEYDOWN:
+      running = False
+
   screen.fill(0) #set pygame screen to black
-  frame=getCamFrame(color,camera)
-  screen=blitCamFrame(frame,screen)
+  cam.blitCamFrame(screen, (0, 0))
   pygame.display.flip()
 cv2.destroyAllWindows()
